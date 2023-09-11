a22947e669ebbc929a9b40d3e733651f0ab3beb3
snpara <Frost_han@outlook.com>
2023-09-11 10:20:48 +0800

    first commit

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,53 @@
-#include <iostream>
+#include <SDL.h>
+#include <SDL2_gfxPrimitives.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_net.h>
+#include <SDL_ttf.h>
 
-auto main() -> int
+int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    //   SDL_Surface *surface;
+    SDL_Event event;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                     "Couldn't initialize SDL: %s",
+                     SDL_GetError());
+        return 3;
+    }
+
+    if (SDL_CreateWindowAndRenderer(320,
+                                    240,
+                                    SDL_WINDOW_RESIZABLE,
+                                    &window,
+                                    &renderer))
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                     "Couldn't create window and renderer: %s",
+                     SDL_GetError());
+        return 3;
+    }
+
+    while (1)
+    {
+        SDL_PollEvent(&event);
+        if (event.type == SDL_QUIT)
+        {
+            break;
+        }
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return 0;
 }
